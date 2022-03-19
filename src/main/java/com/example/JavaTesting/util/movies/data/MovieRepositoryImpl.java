f738782a4c193aee10ed0caf2b2251f985702f21
Diego Iturra <diturra@dparadig.com>
2022-03-19 19:52:14 -0300

    ADD: implementation of Repository for movies

---
+++ src/main/java/com/example/JavaTesting/util/movies/data/MovieRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.example.JavaTesting.util.movies.data;
+
+import com.example.JavaTesting.util.movies.model.Movie;
+
+import java.util.Collection;
+
+public class MovieRepositoryImpl implements MovieRepository{
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return null;
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+}
